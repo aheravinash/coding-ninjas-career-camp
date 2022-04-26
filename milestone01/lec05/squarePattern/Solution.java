a2f3339b173303fc621130b1729dbc356c782c6e
Avinash Aher <aher.avinash80@gmail.com>
2022-04-26 16:21:36 +0530

    add solution to square pattern

---
+++ milestone01/lec05/squarePattern/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Solution{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		s.close();
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				System.out.print(n);
+			}
+			System.out.println();
+		}
+	}
+}
